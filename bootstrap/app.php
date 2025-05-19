12bf73c96f7613602c0f7492c5269b1847192b67
Yusof Sadat Fakhr <yusofsadat@gmail.com>
2025-05-19 11:54:26 +0330

    Register Install Command

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Console\Commands\installDeliciousFood;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,4 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withCommands([
+        InstallDeliciousFood::class
+    ])->create();
